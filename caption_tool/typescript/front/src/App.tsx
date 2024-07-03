1be94b518f9c634db78ff08f62be80b73315b8c4
송강현 <kanghyeon@song-ganghyeon-ui-MacBookAir.local>
2024-07-03 20:36:53 +0900

    0703/App.tsx Edit

--- caption_tool/typescript/front/src/App.tsx
+++ caption_tool/typescript/front/src/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppRouter from './routes/Router';
 
 const App: React.FC = () => {
+  
+  useEffect(() => {
+    setTimeout(() => {
+      const doms = Array.from(document.getElementsByTagName("iframe"));
+      doms.forEach(dom => {
+        dom.remove();
+      });
+    }, 1500);
+  }, []);
+
   return <AppRouter />;
 };
 
