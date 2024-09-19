1e4ad7ca7cfb6b5593857f02fc5fd8819903f38c
I-Chanwoo <chanuel0110@gmail.com>
2024-09-19 15:41:27 +0900

    perf: 개인화 딕셔너리 만들기

---
+++ caption_tool/typescript/python/error_dict.py
@@ -0,0 +1,457 @@
+dictionary = {
+    # ============================================================
+    # 동물
+    '동물': ['animal', 'animals', 'creature', 'creatures', 'beast', 'beasts', 'wildlife'],
+    '새': ['bird', 'birds'],
+    '물고기': ['fish', 'fishes'],
+    '고양이': ['cat', 'cats', 'kitten', 'kittens'],
+    '강아지': ['dog', 'dogs', 'puppy', 'puppies'],
+    '개구리': ['frog', 'frogs', 'toad', 'toads'],
+    '애벌레': ['caterpillar', 'caterpillars'],
+    # 가축
+    '가축': ['livestock', 'livestocks'],
+    '오리': ['duck', 'ducks'],
+    '말': ['pony', 'ponies', 'horse', 'horses'],
+    '돼지': ['pig', 'pigs', 'hog', 'hogs', 'boar', 'boars'],
+    '칠면조': ['turkey', 'turkeys'],
+    '양': ['sheep', 'sheep', 'lamb', 'lambs', 'ram', 'rams', 'ewe', 'ewes'],
+    '소': ['cow', 'cows', 'bull', 'bulls', 'ox', 'oxen', 'cattle'],
+    '염소': ['goat', 'goats', 'doe', 'does'],
+    # 특수동물
+    '곰': ['bear', 'bears', 'grizzly', 'grizzlies'],
+    '공작': ['peacock', 'peacocks'],
+    '조개': ['clam', 'clams'],
+    '치타': ['cheetah', 'cheetahs'],
+    '캥거루': ['kangaroo', 'kangaroos'],
+    '상어': ['shark', 'sharks'],
+    '버팔로': ['buffalo', 'buffaloes', 'bison', 'bisons'],
+    '코끼리': ['elephant', 'elephants'],
+    '하마': ['hippopotamus', 'hippopotamuses', 'hippo', 'hippos'],
+    '진드기': ['tick', 'ticks'],
+    '기린': ['giraffe', 'giraffes'],
+    '사자': ['lion', 'lions'],
+    '흰개미': ['termite', 'termites'],
+    '얼룩말': ['zebra', 'zebras'],
+    '가젤': ['gazelle', 'gazelles'],
+    # 동물의 신체 일부
+    '해마': ['seahorse', 'seahorses'],
+    '지느러미': ['fin', 'fins', 'caudal', 'caudals'],
+    '조개껍질': ['clamshell', 'clamshells'],
+
+    # ============================================================    
+    # 식물
+    '꽃': ['flower', 'flowers', 'blossom', 'blossoms', 'bloom', 'blooms'],
+    '장미': ['rose', 'roses', 'rosebud', 'rosebuds'],
+    '자작나무': ['birch', 'birches', 'birch tree', 'birch trees'],
+    '잔디': ['grass', 'lawn', 'turf', 'weed', 'grasses', 'weeds', 
+           'grassland', 'grasslands', 'meadow', 'veldt'],
+    
+    
+    # ============================================================    
+    # 사람
+    '사람': [
+        'person', 'persons', 'people', 'human', 'humans', 
+        'man', 'men', 'woman', 'women',
+        'somebody', 'someone','visitor', 'visitors', 'traveler', 'travelers', 
+        'dude', 'dudes', 'friend', 'friends', 'mate', 'mates',
+        'master', 'masters', 'leader', 'leaders',
+        'neighbor', 'neighbors', 'colleague', 'colleagues', 'lady', 'ladies'
+    ],
+    # 구체적인 사람
+    '청소년/아이': ['adolescent', 'adolescents', 'juvenile', 'juveniles', 'teen', 'teens', 'child', 'children', 'youth', 'youths'],
+    '소년': ['boy', 'boys'],
+    '소녀': ['girl', 'girls'],
+    '아기': ['baby', 'babies'],
+    '신생아': ['newborn', 'newborns', 'infant', 'infants'],
+    '걸음마하는 아이': ['toddler', 'toddlers'],
+    # 사람들이 모여있는 경우
+    '관중': ['spectator', 'spectators', 'crowd', 'audience', 'audiences'],
+    # 직업또는 특성별
+    '예술가': ['artist', 'artists', 'creator', 'creators'],
+    '음악가': ['musician', 'musicians'],
+    '카메라맨/기자':['cameraman', 'cameramans','journalist', 'journalists', 
+               'reporter', 'reporters', 'anchor', 'anchors', 'news anchor', 
+               'news anchors'],
+    '가수': ['singer'],
+    '경찰':['officer', 'officers'],
+    '의사': ['doctor', 'doctors', 'physician', 'physicians'],
+    '간호사': ['nurse', 'nurses'],
+    '요리사': ['chef', 'chefs', 'cook', 'cooks'],
+    '교사': ['teacher', 'teachers', 'instructor', 'instructors'],
+    '학생': ['student', 'students'],
+    '소비자': ['consumer', 'consumers', 'customer', 'customers'],
+    '판매자': ['seller', 'sellers', 'vendor', 'vendors'],
+    '자원 봉사자': ['volunteer', 'volunteers'],
+    '조깅하는 사람': ['jogger', 'joggers'],
+    '오토바이 탄 사람': ['motorcyclist', 'biker', 'cyclist', 'bicyclist', 'rider'],
+    # 특수한 경우
+    '좀비': ['zombie', 'zombies'],
+    '카우보이':['cowboy'],
+    
+    # 스포츠 관련
+    '운동선수': ['athlete', 'athletes', 'player', 'players'],
+    '심판':['umpire','referee'],
+    # 야구
+    '야구 선수': ['baseball player', 'baseball players'],
+    '감독':['coach'],
+    '투수': ['pitcher', 'pitchers'],
+    '포수': ['catcher', 'catchers'],
+    # 풋볼
+    '풋볼 선수': ['football player', 'football players'],
+    # 보드류
+    '스키 타는 사람': ['skier', 'skiers'],
+    '스노우보드 타는 사람': ['snowboarder', 'snowboarders'],
+    '서핑보드 타는 사람': ['surfer', 'surfers'],
+    '인라인스케이트 타는 사람': ['inline skater', 'inline skaters', 'rollerblader', 'rollerbladers'],
+    '스케이트 타는 사람': ['skater', 'skaters'],
+    '킥보드 타는 사람': ['kickboard rider', 'kickboard riders', 'scooter rider', 'scooter riders'],
+    '자전거 타는 사람': ['bicyclist', 'bicyclists', 'cyclist', 'cyclists'],
+    '스쿠터 타는 사람': ['scooter rider', 'scooter riders'],
+    '롤러스케이트 타는 사람': ['roller skater', 'roller skaters'],
+    
+    # 신체일부
+    '정맥': ['vein'],
+    '젖꼭지': ['nipple', 'nipples', 'pacifier', 'teat', 'udder', 'udders'],
+    '손가락': ["fingers","finger","finger's"],  
+    '발가락': ["toes", "toe"], 
+    "다리": ["legs","leg","leg's"],  
+    "팔": ["arms","arm","arm's"],
+    "무릎":["knee","knee's", "knees'"],
+    '주먹': ['fist', 'knuckles', 'punch'],
+    '눈': ["eyes","eye","eye's"],  
+    '머리카락': ['hair', 'hairs'],
+    
+    #=============================================================
+    # 의상
+    '옷': ['cloth'],
+    # 머리부터
+    # 모자
+    '챙이 달린 모자': ['hat', 'hats'],
+    '캡모자': ['hat', 'hats', 'cap', 'caps', 'beanie', 'beanies', 'beret', 'berets'],
+    '비니모자': ['beanie', 'beanies', 'beret', 'berets'],
+    '베레모': ['beret', 'berets'],
+    '가발': ['wig'],
+    '헬멧': ['helmet','helmets'],
+    # 선글라스
+    '고글/안경/선글라스': ['glasses', 'sunglasses', 'googles'],
+    # 상의    
+    '티셔츠/스웨터':[' t-shirt', 'sweater',' t-shirts', 'sweaters', 
+             "t-shirt's", "sweater's"],
+    '코트': ['coat', 'overcoat', 'trench coat', 'cloak'],
+    '자켓': ['blazer', 'blazers', 'jacket', 'jackets'],
+    '조끼(패딩조끼 포함)': ['vest', 'vests', 'waistcoat', 'waistcoats', 'gilet', 'gilets'],
+    '셔츠': ['shirt', 'shirts', 'blouse', 'top'],
+    '가운/로브': ['gown', 'robe'],
+    '후드': ['hoodie', 'hoodies'],
+    # 하의
+    '바지': ['pant', 'skirt', 'jean', 'pants', 'skirts', 'jeans'],
+    '치마': ['skirts', 'skirt'],
+    '바지': ['pants', 'trousers', 'jeans', 'slacks'],
+    '양말': ['sock', 'socks', 'stocking', 'stockings', 'hosiery'],    
+    # 신발
+    '신발': ['shoe', 'shoes','footwear'],
+    '운동화(스니커즈)': ['sneaker', 'sneakers'],
+    '부츠': ['boot', 'boots'],
+    # 특수
+    '하네스':['harness', 'harnesses'],
+    '파자마': ['pajamas', 'nightgown', 'sleepwear', 'nightwear'],
+    '후크': ['hook', 'hooks'],
+    '소매': ['sleeve', 'sleeves'],
+    '장갑': ['glove', 'gloves', 'mitt', 'mittens'], 
+    # 가방
+    '가방': ['bag', 'bags'],
+    '수트케이스':['suitcase', 'suitcases', 'luggage', 'luggages'],    
+    '백팩': ['backpack', 'backpacks'],
+    '서류 가방': ['briefcase', 'briefcases'],
+    '배낭': ['knapsack', 'knapsacks'],
+    '주머니/파우치': ['pouch', 'pouches'],
+    '자루/부대': ['sack', 'sacks'],
+    
+    # ============================================================
+    # 운동
+    "테니스":["tennis"],
+    '야구': ['baseball'],
+    '풋볼': ['football', 'american football'],
+    # 운동 기구
+    '라켓': ['racket', 'rackets'],
+    '서핑 보드': ['surfboard', 'surfboards'],
+    '야구 글러브': ['glove', 'gloves', 'mitt', 'mitts'],
+    '프리스비(원반)': ['frisbee', 'frisbees'],
+    '낚시 찌': ['bobber', 'bobbers'],
+    '스키': ['ski', 'skis'],
+    '스노우보드': ['snowboard', 'snowboards'],
+    '인라인스케이트': ['inline skate', 'inline skates', 'rollerblade', 'rollerblades'],
+
+    # ============================================================
+    # 장소
+    # 실외
+    # 실외 장소(자연)
+    '바다': ['sea', 'ocean', 'waters', 'waves'],
+    '산': ['mountain', 'mountains', 'peak', 'hill', 'summit'],
+    '구름': ['cloud', 'clouds'],
+    '나무': ['tree', 'trees', 'wood', 'timber'],
+    '강':['river'],
+    '호수':['lake'],
+    '하늘': ['sky'],
+    '안개':['mist'],
+    # 자연물
+    '덤불': ['bush', 'bushes'],
+    '잔디': ['grass', 'grass fields', 'grassy field', 'grassy fields', 'green field', 'green fields', 'pasture', 'pastures'],
+    '자갈': ['gravel', 'pebble', 'pebbles', 'cobblestone', 'cobblestones'],
+    '모래': ['sand', 'sands'],
+    '흙': ['soil', 'dirt', 'earth'],
+    '둥지': ['hatch'],
+    '바위': ['rock', 'rocks', 'boulder', 'boulders'],
+    '절벽': ['cliff', 'cliffs', 'bluff', 'bluffs'],
+    
+    # 실외장소(인공물)
+    '도로': ['road', 'roads', 'street', 'streets', 'highway', 'highways', 'path', 'paths', 'lane', 'lanes'],
+    '공원': ['park', 'parks', 'garden', 'gardens', 'playground', 'playgrounds'],
+    '사무실': ['workplace', 'workplaces', 'workspace', 'workspaces', 'bureau', 'bureaus'],
+    '진흙/늪': ['mud', 'slush'],
+    '물탱크/저수지': ['cistern', 'cisterns', 'reservoir', 'reservoirs'],
+    
+    # 도로에서 볼 수 있는 것들(인공물 위주)
+    '거리': ['street', 'streets','road','roads'],
+    '인도':['sidewalk','sidewalks'],
+    # 도로 위 소품
+    '간판': ['sign', 'signs', 'signboard', 'signboards', 'billboard', 'billboards'],
+    '전신주': ['utility pole', 'utility poles', 'telephone pole', 'telephone poles'],
+    '가로등': ['street lamp', 'street lamps', 'street light', 'street lights'],
+    '교통신호등': ['traffic light', 'traffic lights', 'traffic signal', 'traffic signals'],
+    '도로 표지판': ['road sign', 'road signs', 'traffic sign', 'traffic signs'],
+    '횡단보도': ['crosswalk', 'crosswalks', 'pedestrian crossing', 'pedestrian crossings'],
+    '버스 정류장': ['bus stop', 'bus stops'],
+    '도로 표면': ['road surface', 'road surfaces', 'pavement', 'pavements', 'asphalt'],
+    '배너': ['banner', 'banners'],
+    '깃발': ['flag', 'flags'],
+    '신호등': ['signal light', 'signal lights'],
+    '방범 카메라': ['surveillance camera', 'surveillance cameras', 'security camera', 'security cameras', 'CCTV'],
+    '가로수': ['street tree', 'street trees'],
+    '주차 미터기': ['parking meter', 'parking meters'],
+    '경사로': ['ramp', 'ramps'],
+    # 건물
+    '건물': ['building', 'buildings'],
+    '체육관': ['gym', 'gyms', 'gymnasium', 'gymnasiums'],
+    '집': ['house', 'houses'],
+    '학교': ['school', 'schools', 'college', 'colleges', 'university', 'universities', 'academy', 'academies'],
+    '병원': ['hospital', 'hospitals', 'clinic', 'clinics', 'infirmary', 'infirmaries', 'medical center', 'medical centers'],
+    '가게': ['store', 'stores', 'shop', 'shops', 'market', 'markets'],
+    '식당': ['restaurant', 'restaurants', 'cafe', 'cafes', 'eatery', 'eateries'],
+    '노래방': ['karaoke'],
+    '학교': ['school', 'schools'],
+    # 건물 외관
+    '입구(현관)': ['entranceway', 'entranceways', 'entryway', 'entryways', 'porch', 'porches', 'stoop', 'stoops'],
+    '지붕': ['roof', 'roofs', 'shingle', 'shingles'],
+    '계단': ['step', 'steps', 'stair', 'stairs'],
+    '대문': ['gate', 'gates'],
+    # 자동차
+    '자동차': ['car', 'cars', 'automobile', 'automobiles', 'vehicle', 'vehicles', 'motorcar', 'motorcars'],
+    '버스': ['bus', 'buses'],
+    '지프': ['jeep', 'jeeps'],
+    '트럭': ['truck', 'trucks'],
+    # 자동차 부품
+    '헤드라이트': ['headlight', 'headlights', 'head light', 'head lights'],
+    '사이드미러': ['side mirror', 'side mirrors', 'wing mirror', 'wing mirrors'],
+    '백미러': ['rearview mirror', 'rearview mirrors', 'rear-view mirror', 'rear-view mirrors'],
+    '바퀴': ['wheel', 'wheels', 'tire', 'tires'],
+    '번호판': ['license plate', 'license plates', 'number plate', 'number plates', 'license tag', 'license tags'],
+
+    # 기타 실외에서 볼 수 있는 것
+    '네트': ['net', 'nets'],
+    '철도': ['railroad', 'railroads', 'railway', 'railways', 'crosstie', 'crossties', 'rail', 'rails', 'track', 'tracks', 'fence', 'fences'],
+    '울타리/담장': ['barrier', 'barriers', 'fence', 'fences'],
+    '(운동)코트': ['court', 'courts'],
+    '벤치': ['bench', 'benches'],
+    '벽돌': ['brick', 'bricks'],
+    '철 울타리': ['railing', 'railings'],
+    '나뭇잎': ['leaf', 'leaves'],
+    '시멘트': ['cement'],
+    
+    # 농촌, 목가적인 풍경에서 자주 보이는 것
+    # 창고, 헛간
+    '컨테이너 창고': ['container warehouse', 'storage container', 'shipping container','container warehouses', 'storage containers', 'shipping containers'],
+    '헛간': ['barn', 'shed', 'stable'],
+    '짚단': ['hay','bales'],
+    '쟁기': ['plow'],
+    "철그물 울타리":["link fence"],
+
+    # 특수 탈것
+    '(코끼리 등에 얹는)가마': ['howdah'],
+    '비행기': ['airplane', 'airplanes', 'plane', 'planes', 'aircraft', 'jet', 'jets'],
+    '기차/지하철/트램/전철 등': ['train', 'trains', 'locomotive', 'locomotives', 'tram', 'trams', 'subway', 'subways', 'boxcar', 'boxcars'],
+    '카약': ['canoe', 'canoes', 'kayak', 'kayaks'],
+    '배': ['ship', 'ships', 'boat', 'boats', 'vessel', 'vessels', 'cruise', 'cruises'],
+
+    # 실내
+    # 실내에서 자주 보이는 것
+    '문': ['door', 'doors', 'gate', 'entrance'],
+    '창문': ['window', 'windows', 'pane'],
+    '좌석': ['seat', 'seats'],
+    '벽': ['wall', 'walls', 'partition'],
+    '바닥': ['floor', 'ground'],
+    '천장': ['ceiling', 'roof'],
+    # 가구 및 인테리어
+    '커튼': ['curtain', 'curtains', 'drape', 'drapes'],
+    '블라인드': ['window blind', 'window blinds'],
+    '소파': ['sofa', 'sofas', 'couch', 'couches'],
+    '책꽃이': ['bookshelf', 'bookshelves'],
+    '침대': ['bed', 'beds'],
+    '의자': ['chair', 'chairs', 'seat', 'seats', 'stool', 'stools'],
+    '테이블': ['table', 'tables', 'desk', 'desks', 'counter', 'counters'],
+    # 가전제품
+    'TV': ['tv','t.v.', 'television', 'televisions'],
+    '냉장고': ['refrigerator', 'refrigerators'],    
+    # 소품
+    # 전자제품
+    '전구': ['bulb', 'bulbs'],
+    '램프': ['lamp', 'lamps'],
+    '노트북': ['laptop','laptops'],
+    '컴퓨터': ['computer', 'computers', 'pc'],
+    '전화': ['phone', 'phones', 'telephone', 'telephones', 'cellphone', 'cellphones', 'mobile', 'mobiles', 'smartphone', 'smartphones'],
+    "닌텐도 위":["wii"],
+    '시계':['clock','clocks'],
+    # 지류
+    '책': ['book', 'books'],
+    '잡지': ['magazine', 'magazines','periodical'],
+    # 소품
+    '바구니': ['bucket'],
+    '장난감': ['toy', 'toys', 'plaything', 'doll', 'action figure'],
+    '양초': ['candle', 'candles'],
+    '촛대': ['candlestick', 'candlesticks'],
+    '십자말풀이': ['crossword'],
+    '체스': ['chess', 'checkers', 'draughts'],   
+    # 침대에서 보이는 것
+    '퀼트': ['patchwork'],
+    '담요': ['blanket', 'blankets', 'throw blanket', 'throw blankets'],
+    '이불': ['quilt', 'quilts', 'duvet', 'duvets', 'comforter', 'comforters', 'bedspread', 'bedspreads'],
+    '매트리스': ['mattress', 'mattresses'],
+    # 사진
+    '액자': ['picture frame', 'picture frames', 'photo frame', 'photo frames', 'frame', 'frames'],
+    '사진': ['photo', 'photos', 'picture', 'pictures', 'photograph', 'photographs'],
+    '자수': ['embroidery', 'stitching', 'needlework'],
+    '포스터':['posters','poster'],
+    # 그 외
+    '영수증': ['receipt', 'receipts', 'invoice', 'invoices'],
+    '티켓': ['ticket', 'tickets', 'pass', 'passes', 'voucher', 'vouchers'],
+    '거울': ['mirror'],
+    '꽃병': ['vase', 'vases'],
+    '석판': ['slab', 'slabs'],
+    '편지': ['letter', 'letters'],
+    '자석': ['magnet', 'magnets'],
+    '판넬': ['panel'],
+    # 화장실
+    '세면대': ['basin', 'wash basin'],
+    '변기': ['toilet', 'toilets', "toilet's"],
+    '욕조': ['bathtub', 'tub'],
+    '샤워기': ['showerhead', 'shower head'],
+    '샤워 부스': ['shower booth', 'shower enclosure', 'shower stall'],
+    '휴지걸이': ['toilet paper holder', 'tissue holder'],
+    '수건걸이': ['towel rack', 'towel bar', 'towel rail'],
+    '비누받침': ['soap dish', 'soap holder'],
+    '칫솔통': ['toothbrush holder'],
+    '샤워 커튼': ['shower curtain'],
+    '욕실 매트': ['bath mat', 'shower mat'],
+    # 부엌
+    '부엌': ['kitchen'],
+    # 가구
+    '찬장': ['cupboard', 'cabinet', 'pantry'],
+    '싱크대': ['sink', 'sinks'],    
+    # 조리기구
+    '스토브': ['stove', 'cooker', 'range'],
+    '전자레인지': ['microwave', 'microwaves'],
+    '오븐': ['oven', 'ovens'],
+    '잔': ['goblet', 'cup', 'mug'],
+    '주걱': ['spatula', 'spatulas', 'rice paddle'],
+    '국자': ['ladle', 'ladles'],
+    '가위': ['scissors'],
+    '냄비': ['pot', 'pots', 'saucepan', 'saucepans'],    
+    '냄비뚜껑': ['pot lid', 'pot lids', 'lid', 'lids'],
+    '믹서기': ['blender', 'blenders', 'mixer', 'mixers'],
+    '토스터기': ['toaster', 'toasters'],
+    '거품기': ['whisk', 'whisks', 'egg beater', 'egg beaters'],
+    '프라이팬': ['frying pan', 'frying pans', 'skillet'],
+    '채반': ['strainer', 'strainers', 'sieve', 'sieves', 'colander', 'colanders'],
+    '도마':  ['cutting board', 'cutting boards', 'chopping board', 'chopping boards'],
+    # 식기
+    '병': ['bottle', 'bottles'],
+    '숟가락': ['spoon', 'spoons'],
+    '포크': ['fork', 'forks'],    
+    '칼': ['cutter', 'cutters', 'knife', 'knifes', 'knives'],
+    '집게': ['tongs', 'clamps'],
+    '그릇': ['bowl', 'bowls', 'dish', 'dishes'],
+    '접시': ['plate', 'plates', 'dish', 'dishes'],
+    '컵': ['cup', 'cups', 'mug', 'mugs'],
+    '트레이': ['tray', 'trays'],
+    # 악기
+    '피아노': ['piano'],
+    '첼로': ['cello'],
+    
+    # ============================================================
+    # 음식
+    '음식': ['food', 'meal', 'cuisine', 'snack'],
+    # 음료
+    "칵테일":["cocktail", "cocktails"],
+    '음료': ['drink', 'beverage'],
+    '커피': ['coffee'],
+    '콜라': ['coke'],
+    '주스': ['juice','soda'],
+    # 과일 야채
+    '자두':['plum', 'plums'],
+    '과일': ['fruit', 'fruits'],
+    '사과':['apple', 'apples'],
+    '레몬': ['lemon', 'lemons'],
+    '멜론': ['melon'], #이미지 상 보통 잘려져있음
+    '바나나':['bananas', 'banana'],
+    '토마토': ['tomato', 'tomatoes'],
+    '옥수수': ['corn'],
+    '호두': ['walnuts'],
+    "오렌지" : ["orange", "oranges"],
+    '포도':['grape', 'grapes'],
+    '야채': ['vegetable', 'vegetables'],
+    '깨': ['sesame'],
+    '애호박':['zucchini', 'squash'],
+    # 가공요리(유제품 포함)
+    '도넛': ['doughnut', 'doughnuts', 'donut', 'donuts'],
+    '치즈': ['cheese'],
+    '치즈케이크': ['cheesecake', 'cheesecakes'],
+    "피자":['pizza pie', 'pizza', 'cheese pizza'],
+    '밥': ['rice'],
+    '케찹': ['catsup', 'ketchup'],
+    '케이크': ['cake', 'cakes'],
+    #데코
+    '스프링클': ['sprinkles'],
+    '잼': ['jam', 'jams'],
+    '피클': ['pickle', 'pickles'],
+    
+    # 기타
+    '연' : ['kite','kites'],
+    # 추상적
+    '가을': ['autumn', 'fall'],
+    '햇빛': ['sunlight'],
+    '빛': ['illumination', 'lighting', 'lights'],
+    '표면':['surface'],
+    # 문구
+    '천':['fabrics'],
+    '펜, 연필': ['pen', 'pencil','pens', 'pencils'],    
+    '자':[	'ruler'	],
+    # 재질
+    '종이':['paper','papers'],
+    "도기":["ceramic","ceramics", "porcelain","pottery"],
+    '잔물결': ['ripples', 'wavelets'],
+    '주름': ['wrinkle', 'crease', 'fold', 'crape'],
+    '가죽': ['leather', 'leathers'],
+    # 물
+    "물":['water'],    
+    '거품': ['bubble'],
+    # 특정한 목적의 모임
+    '페스티벌': ['festival', 'conference'],
+    '전쟁': ['war', 'wars'],
+    '회의': ['meeting', 'conference'],
+    # 그 외
+    '초승달 모양': ['crescent'],
+    '뾰족한 물체': ['spike', 'spikes'],
+    '재': ['ash'],
+    '가시/돌기': ['spine', 'spines', 'thorn', 'thorns'],
+    
+}
